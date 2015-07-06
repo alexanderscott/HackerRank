702701888cc87de3b96a27f727140d28f67b690c
Alex Ehrnschwender <alexanderscott46@gmail.com>
2015-07-05 19:31:45 -0700

    hello world and basic data types in c++

---
+++ C++/basic-data-types.cpp
@@ -0,0 +1,16 @@
+// https://www.hackerrank.com/challenges/c-tutorial-basic-data-types
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    string s;
+    for(int i=0; i < 6; i++) {
+        cin >> s;
+        cout << s << endl;
+    }
+    
+    return 0;
+}
+

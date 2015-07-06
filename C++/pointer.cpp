a27ce212914068849a12caf1550c85afcd54b56d
Alex Ehrnschwender <alexanderscott46@gmail.com>
2015-07-05 20:03:57 -0700

    c++ pointer

---
+++ C++/pointer.cpp
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/c-tutorial-pointer
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void update(int *a,int *b) {
+    int c = *a + *b;
+    int d = abs(*a - *b);
+    *a = c;
+    *b = d;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
